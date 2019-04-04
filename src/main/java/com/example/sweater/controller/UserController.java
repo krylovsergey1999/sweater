f709aa284c1ea106e56c99b86a12ba0a475dc4c9
Krylov Sergey <kssergeyru@gmail.com>
2019-04-04 11:45:45 +0300

    lesson6.1 Добавлена страница с user

---
+++ src/main/java/com/example/sweater/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping
+    public String userList(Model model) {
+        model.addAttribute("users",userRepo.findAll());
+        return "userList";
+    }
+}
--- src/main/java/com/example/sweater/domain/Role.java
+++ src/main/java/com/example/sweater/domain/Role.java
@@ -2,8 +2,9 @@ package com.example.sweater.domain;
 
 import org.springframework.security.core.GrantedAuthority;
 
+// Granted Authority - Предоставленные полномочия
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {
